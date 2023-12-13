109a50a78e8b7c2ed14b3637ef2b398af5d70913
AntoineGouzy <antoinegp.gouzy@gmail.com>
2023-12-13 16:34:56 +0100

    Couverture par les tests de 97%

--- src/test/java/iut/info3/betterstravaapi/user/UserServiceTest.java
+++ src/test/java/iut/info3/betterstravaapi/user/UserServiceTest.java
@@ -1,4 +1,25 @@
+package iut.info3.betterstravaapi.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class UserServiceTest {
-  
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testCheckPresenceEmailWithEmailAlreadyTaken() {
+        assertTrue(userService.checkPresenceEmail("utilisateur@test.com"));
+    }
+
+    @Test
+    public void testCheckPresenceEmailWithoutEmailAlreadyTaken() {
+        assertFalse(userService.checkPresenceEmail("utilisateurnonexistant@test.com"));
+    }
+
 }
