498181d9d8396376cce40ebd69d9d1fbe5692aa7
ANTOINE LALA <antoine.lala@iut.local>
2023-12-13 17:19:50 +0100

    creation du test, a completer

--- src/test/java/iut/info3/betterstravaapi/user/UserServiceTest.java
+++ src/test/java/iut/info3/betterstravaapi/user/UserServiceTest.java
@@ -22,4 +22,20 @@ class UserServiceTest {
         assertFalse(userService.checkPresenceEmail("utilisateurnonexistant@test.com"));
     }
 
+    @Test
+    public void testCreationTokenValid() {
+        UserEntity entity =
+                new UserEntity("John.Doe@gmail.com","John","Doe","mdp");
+        entity.setId(1234);
+        String expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                + "eyJpZCI6MTIzNCwiZW1haWwiOiJKb2huLkRvZUBnbWFpbC5jb20iLCJkYXRldGltZS1jbGFpbSI6MTcwMjQ4MzQzMSwiZXhwIjoxNzA1MDc1NDMxfQ." +
+                "ptepiOaary3v_kOlx0syVEEmYZ7a1B2bl1UBQK9SuL8";
+        String[] tabExpected = expectedToken.split("\\.");
+        String[] tabUser = userService.generateToken(entity).split("\\.");
+
+        assertEquals(tabExpected[0],tabUser[0]);
+        assertEquals(tabExpected[2],tabUser[2]);
+
+    }
+
 }
