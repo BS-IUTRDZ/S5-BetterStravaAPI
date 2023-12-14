dd26a6832c2c220612e04e077009bfac56e417dc
ANTOINE LALA <antoine.lala@iut.local>
2023-12-14 16:17:11 +0100

    changement generate token + test 100% sur tout

--- src/main/java/iut/info3/betterstravaapi/user/UserService.java
+++ src/main/java/iut/info3/betterstravaapi/user/UserService.java
@@ -12,6 +12,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -33,21 +34,17 @@ public class UserService {
         return user != null;
     }
 
-    public List<UserEntity> findAll() {
-        return userRepository.findAll();
-    }
-
     public List<UserEntity> findByEmailAndPassword(String email, String password) {
         return userRepository.findByEmailAndPassword(email,password);
     }
 
-    public String generateToken(UserEntity user) {
+    public String generateToken(UserEntity user, Instant currentDate) {
         Algorithm algorithm = Algorithm.HMAC256(secretSentence);
         String jwt = JWT.create()
                 .withClaim("id", user.getId() )
                 .withClaim("email", user.getEmail())
-                .withClaim("datetime-claim", Instant.now())
-                .withExpiresAt(Instant.now().plus(tokenExpirationDuration, ChronoUnit.SECONDS))
+                .withClaim("datetime-claim", currentDate)
+                .withExpiresAt(currentDate.plus(tokenExpirationDuration, ChronoUnit.SECONDS))
                 .sign(algorithm);
         return jwt;
     }
--- src/test/java/iut/info3/betterstravaapi/user/UserServiceTest.java
+++ src/test/java/iut/info3/betterstravaapi/user/UserServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Instant;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -23,18 +25,25 @@ class UserServiceTest {
     }
 
     @Test
+    public void testFindUserInDataBase() {
+        UserEntity rechercher = new UserEntity("utilisateur@test.com","test","utilisateur","9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+        rechercher.setId(1);
+        assertEquals(rechercher.toString(),userService.findByEmailAndPassword("utilisateur@test.com","9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08").get(0).toString());
+    }
+
+    @Test
     public void testCreationTokenValid() {
+        Instant date = Instant.parse("2023-11-30T18:35:24.00Z");
         UserEntity entity =
                 new UserEntity("John.Doe@gmail.com","John","Doe","mdp");
         entity.setId(1234);
-        String expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
-                + "eyJpZCI6MTIzNCwiZW1haWwiOiJKb2huLkRvZUBnbWFpbC5jb20iLCJkYXRldGltZS1jbGFpbSI6MTcwMjQ4MzQzMSwiZXhwIjoxNzA1MDc1NDMxfQ." +
-                "ptepiOaary3v_kOlx0syVEEmYZ7a1B2bl1UBQK9SuL8";
+        String expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+                "eyJpZCI6MTIzNCwiZW1haWwiOiJKb2huLkRvZUBnbWFpbC5jb20iLCJkYXRldGltZS1jbGFpbSI6MTcwMTM2OTMyNCwiZXhwIjoxNzAzOTYxMzI0fQ." +
+                "rliw-Dr4ZkzXLClkE-EsbF7lyUcoK2Q-TxncyEFhq4k";
         String[] tabExpected = expectedToken.split("\\.");
-        String[] tabUser = userService.generateToken(entity).split("\\.");
+        String[] tabUser = userService.generateToken(entity,date).split("\\.");
 
-        assertEquals(tabExpected[0],tabUser[0]);
-        assertEquals(tabExpected[2],tabUser[2]);
+        assertEquals(expectedToken,userService.generateToken(entity,date));
 
     }
 
