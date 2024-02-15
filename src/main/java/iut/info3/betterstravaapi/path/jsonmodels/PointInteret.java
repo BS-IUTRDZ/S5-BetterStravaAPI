b4b1526c93070cb5e26e5048e38a4358fdd54963
ClemFR <clementlaurie27@gmail.com>
2024-02-15 15:20:11 +0100

    Test du json d'entrée pour la création des paths

--- src/main/java/iut/info3/betterstravaapi/path/jsonmodels/PointInteret.java
+++ src/main/java/iut/info3/betterstravaapi/path/jsonmodels/PointInteret.java
@@ -26,4 +26,8 @@ public class PointInteret {
                 '}';
     }
 
+    public Point getPos() {
+        return pos;
+    }
+
 }
---
+++ src/test/java/iut/info3/betterstravaapi/path/jsonmodels/FullPathTest.java
@@ -0,0 +1,69 @@
+package iut.info3.betterstravaapi.path.jsonmodels;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FullPathTest {
+
+    private FullPath path;
+
+    @BeforeEach
+    void setUp() {
+
+        String pathJson = "{\"nom\":\"parcours de test\",\"description\":\"description du parcours\",\"date\":1708005866,\"points\":[{\"lat\":24.7162,\"lon\":-12.7261,\"alt\":1330.62}],\"pointsInterets\":[{\"pos\":{\"lat\":1,\"lon\":2,\"alt\":3},\"nom\":\"points d'interet\",\"description\":\"description du point d'interet\"}],\"duree\":1996}";
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            path = mapper.readValue(pathJson, FullPath.class);
+        } catch (Exception e) {
+            fail("Impossible de creer le parcours de test");
+        }
+
+    }
+
+    @Test
+    void getNom() {
+        assertEquals("parcours de test", path.getNom());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("description du parcours", path.getDescription());
+    }
+
+    @Test
+    void getDate() {
+        assertEquals(1708005866, path.getDate());
+    }
+
+    @Test
+    void getDuree() {
+        assertEquals(1996, path.getDuree());
+    }
+
+    @Test
+    void getPoints() {
+        assertEquals(1, path.getPoints().size());
+    }
+
+    @Test
+    void pointsToCoordonnees() {
+        assertEquals(1, path.pointsToCoordonnees().size());
+
+        assertEquals(24.7162, path.pointsToCoordonnees().get(0).getLatitude());
+        assertEquals(-12.7261, path.pointsToCoordonnees().get(0).getLongitude());
+        assertEquals(1330.62, path.pointsToCoordonnees().get(0).getAltitude());
+    }
+
+    @Test
+    void getPointsInteret() {
+        assertEquals(1, path.getPointsInteret().size());
+
+        assertEquals(1, path.getPointsInteret().get(0).getPos().toCoordonnees().getLatitude());
+        assertEquals(2, path.getPointsInteret().get(0).getPos().toCoordonnees().getLongitude());
+        assertEquals(3, path.getPointsInteret().get(0).getPos().toCoordonnees().getAltitude());
+    }
+}
